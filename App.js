51cbf1b778625e4cae678d4d265b5872a3c282a4
andy091045 <55223687+andy091045@users.noreply.github.com>
2020-03-09 19:17:02 +0800

    123456

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>123</Text>
+      <Text>123456</Text>
     </View>
   );
 }
